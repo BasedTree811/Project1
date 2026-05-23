40b9eed6d3892b6ee968bcb6fd67a12afabdbedc
BasedTree811 <facetar5557@gmail.com>
2026-05-24 01:32:15 +0800

    Добавлен метод авторизации в ApiService

--- app/lib/services/api_service.dart
+++ app/lib/services/api_service.dart
@@ -3,8 +3,13 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
 
+  // Для Android Emulator
   static const String baseUrl =
       "http://10.0.2.2/library_api";
+
+  // =========================
+  // REGISTRATION
+  // =========================
 
   static Future registerUser({
     required String surname,
@@ -14,14 +19,42 @@ class ApiService {
     required String password,
   }) async {
 
-    var url = Uri.parse("$baseUrl/register.php");
+    var url = Uri.parse(
+      "$baseUrl/register.php",
+    );
 
     var response = await http.post(
       url,
+
       body: {
         "surname": surname,
         "name": name,
         "email": email,
+        "login": login,
+        "password": password,
+      },
+    );
+
+    return jsonDecode(response.body);
+  }
+
+  // =========================
+  // LOGIN
+  // =========================
+
+  static Future loginUser({
+    required String login,
+    required String password,
+  }) async {
+
+    var url = Uri.parse(
+      "$baseUrl/login.php",
+    );
+
+    var response = await http.post(
+      url,
+
+      body: {
         "login": login,
         "password": password,
       },
