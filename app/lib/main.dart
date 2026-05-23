0dafb46aaa61ab41c46a3a246c401b67260b8b43
BasedTree811 <facetar5557@gmail.com>
2026-05-24 01:35:06 +0800

    Подключен экран авторизации

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/register_screen.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
 
-      home: const RegisterScreen(),
+      home: const LoginScreen(),
     );
   }
 }
